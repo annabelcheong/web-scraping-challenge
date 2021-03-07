1edadabbc637cf994b2b851211b19281d719e620
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-03-07 22:49:21 +0800

    Returned the feature image but need to resize

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -32,28 +32,28 @@ def scrape_info():
     #for html home page
     news_p = soup.find('div', class_='article_teaser_body').text
 
-    # ## Main Feature Image ##
-    # # Using splinter and retrieving content from url
-    # url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
-    # browser.visit(url)
+    ## Main Feature Image ##
+    # Using splinter and retrieving content from url
+    url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    browser.visit(url)
 
-    # time.sleep(1)
+    time.sleep(1)
 
-    # # Scrape page into Soup
-    # html = browser.html
-    # soup = bs(html, "html.parser")
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
 
-    # # Attain full url for the image (via inspect on a chrome browser)
-    # # image_url: url_home + relative_image_path
+    # Attain full url for the image (via inspect on a chrome browser)
+    # image_url: url_home + relative_image_path
 
-    # # url_home
-    # url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/'
-    # # relative_image_path
-    # relative_image_path = soup.find('img',class_='headerimage')["src"]
+    # url_home
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/'
+    # relative_image_path
+    relative_image_path = soup.find('img',class_='headerimage')["src"]
     
-    # # featured_image_url
-    # #for html home page
-    # featured_image_url = url + relative_image_path 
+    # featured_image_url
+    #for html home page
+    featured_image_url = url + relative_image_path 
 
     # ## Pandas mars_data_table ##
 
@@ -98,8 +98,8 @@ def scrape_info():
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
-        "news_p": news_p#,
-    # "featured_image_url": featured_image_url,
+        "news_p": news_p,
+        "featured_image_url": featured_image_url#,
     # "html_table":  html_table,
     # "hemisphere_image_urls": hemisphere_image_urls
     }
