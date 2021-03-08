e83fb8082e317081656392b902457d2f0d416d3c
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-03-08 12:55:08 +0800

    trying to push images back to local page

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -55,53 +55,53 @@ def scrape_info():
     #for html home page
     featured_image_url = url + relative_image_path 
 
-    # ## Pandas mars_data_table ##
+    ## Pandas mars_data_table ##
 
-    # # URL
-    # url = 'https://space-facts.com/mars/'
+    # URL
+    url = 'https://space-facts.com/mars/'
 
-    # # Read url(html) with pandas
-    # tables = pd.read_html(url)
+    # Read url(html) with pandas
+    tables = pd.read_html(url)
 
-    # df = tables[0]
+    df = tables[0]
 
-    # # Rename columns
-    # df.columns = ['', 'Mars']
+    # Rename columns
+    df.columns = ['', 'Mars']
 
-    # # Convert table to an html string
-    # html_table = df.to_html()
+    # Convert table to an html string
+    html_table = df.to_html()
     
-    # # Clean up html and remove the \n
-    # #for html home page
-    # html_table = html_table.replace('\n', '')
+    # Clean up html and remove the \n
+    #for html home page
+    html_table = html_table.replace('\n', '')
     
 
-    # ## Four Hemisphere Images ##
+    ## Four Hemisphere Images ##
 
-    # # # Using splinter and retrieving content from url
-    # # url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
-    # # browser.visit(url)
+    # # Using splinter and retrieving content from url
+    # url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
+    # browser.visit(url)
 
-    # # time.sleep(1)
+    # time.sleep(1)
 
-    # # # Scrape page into Soup
-    # # html = browser.html
-    # # soup = bs(html, "html.parser")
+    # # Scrape page into Soup
+    # html = browser.html
+    # soup = bs(html, "html.parser")
 
-    # hemisphere_image_urls = [
-    # {"title": "Valles Marineris Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif"},
-    # {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif"},
-    # {"title": "Schiaparelli Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif"},
-    # {"title": "Syrtis Major Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif"},
-    # ]
+    hemisphere_image_urls = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif"},
+    {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif"},
+    {"title": "Schiaparelli Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif"},
+    {"title": "Syrtis Major Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif"},
+    ]
 
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
         "news_p": news_p,
-        "featured_image_url": featured_image_url#,
-    # "html_table":  html_table,
-    # "hemisphere_image_urls": hemisphere_image_urls
+        "featured_image_url": featured_image_url,
+        # "html_table":  html_table,
+        "hemisphere_image_urls": hemisphere_image_urls
     }
 
     # print(mars_data)
